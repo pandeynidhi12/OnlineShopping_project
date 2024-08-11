c5ebe06a32a968b2a1b8267bae16eac3fb008166
nidhi pandey <nidhipandeyji22@gmail.com>
2024-08-11 08:00:26 +0530

    11aug

---
+++ store.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+from tkinter import ttk
+top=Tk()
+top.geometry('1500x600')        #1200:width,height:600
+top.config(bg='green')
+top.title('welcome')
+
